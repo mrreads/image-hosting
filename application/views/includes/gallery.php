aff144a2db03e743111e4020e4fcffbdbd5ddd60
Alexander <mrreads@yandex.com>
2020-07-27 21:58:19 +0300

    stylize gallery | add images

--- application/views/includes/gallery.php
+++ application/views/includes/gallery.php
@@ -1,12 +1,12 @@
-<div class="galery">
+<div class="gallery">
+    <div class="container">
+        <?php
+        require_once __DIR__ . './../../../vendor/autoload.php';
+        use \Application\Core\Connection;
+        $images = Connection::queryAll("SELECT * from `images`");
 
-<?php
-require_once __DIR__ . './../../../vendor/autoload.php';
-use \Application\Core\Connection;
-$images = Connection::queryAll("SELECT * from `images`");
-
-foreach($images as $image): ?>
-    <img src="/uploads/<?=$image['image_path']?>">
-<?php endforeach; ?>
-
+        foreach($images as $image): ?>
+            <img src="/uploads/<?=$image['image_path']?>">
+        <?php endforeach; ?>
+    </div>
 </div>
